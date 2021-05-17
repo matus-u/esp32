74423326964290f32817ddbe9c5f5dbb827f024b
Matus Ujhelyi <ujhelyi.m@gmail.com>
2021-05-17 23:47:34 +0200

    first attempt to easy rework

--- CameraQrCode/CameraQrCode.ino
+++ CameraQrCode/CameraQrCode.ino
@@ -1,41 +1,94 @@
-#include <Arduino.h>
 #include "ESP32QRCodeReader.h"
 #include <WiFi.h>
 
-ESP32QRCodeReader reader(CAMERA_MODEL_AI_THINKER);
-//ESP32QRCodeReader reader(CAMERA_MODEL_AI_THINKER, FRAMESIZE_SVGA);
+#include "esp_camera.h"
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
 
 void startCameraServer();
+void qrCodeDetectTask();
+static camera_config_t config;
 
-void onQrCodeTask(void *pvParameters)
+static ESP32QRCodeReader *reader = NULL;
+
+void loopA(void* taskData)
 {
-  struct QRCodeData qrCodeData;
+
+  reader = new ESP32QRCodeReader();
 
   while (true)
   {
-    if (reader.receiveQrCode(&qrCodeData, 100))
+    camera_fb_t *fb = esp_camera_fb_get();
+
+    if (!fb)
     {
-      Serial.println("Found QRCode");
-      if (qrCodeData.valid)
-      {
-        Serial.print("Payload: ");
-        Serial.println((const char *)qrCodeData.payload);
-      }
-      else
-      {
-        Serial.print("Invalid: ");
-        Serial.println((const char *)qrCodeData.payload);
-      }
+      Serial.println("Camera capture failed");
+      return;
     }
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    
+    if (reader->qrCodeDetectTask(&config, fb))
+    {
+      Serial.println("DETECTED:");
+      Serial.println((const char *)reader->buffer);
+    }
+
+    esp_camera_fb_return(fb);
+    fb = NULL;
+
+    delay(100);
   }
 }
+
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
-  /*
+
+  if (!psramFound())
+  {
+    Serial.println("No psram");
+    return;
+  }
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_GRAYSCALE;
+
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 15;
+  config.fb_count = 1;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
   WiFi.begin("TP-LINK_B2A4", "910426mativa");
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -50,20 +103,9 @@ void setup()
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
-  */
 
-  reader.setup();
+  xTaskCreatePinnedToCore(loopA, "qrCodeDetectTask", 40 * 1024, NULL, 5, NULL, 1);
 
-  Serial.println("Setup QRCode Reader");
-
-  //reader.setDebug(true);
-  reader.beginOnCore(1);
-
-  Serial.println("Begin on Core 1");
-
-  xTaskCreate(onQrCodeTask, "onQrCode", 4 * 1024, NULL, 4, NULL);
-
-  
 }
 
 void loop()
--- CameraQrCode/ESP32QRCodeReader.h
+++ CameraQrCode/ESP32QRCodeReader.h
@@ -1,63 +1,28 @@
 #ifndef ESP32_QR_CODE_ARDUINO_H_
 #define ESP32_QR_CODE_ARDUINO_H_
 
-#include "Arduino.h"
-#include "ESP32CameraPins.h"
+#include "quirc.h"
 #include "esp_camera.h"
-
-#ifndef QR_CODE_READER_STACK_SIZE
-#define QR_CODE_READER_STACK_SIZE 40 * 1024
-#endif
-
-#ifndef QR_CODE_READER_TASK_PRIORITY
-#define QR_CODE_READER_TASK_PRIORITY 5
-#endif
-
-enum QRCodeReaderSetupErr
-{
-  SETUP_OK,
-  SETUP_NO_PSRAM_ERROR,
-  SETUP_CAMERA_INIT_ERROR,
-};
-
-/* This structure holds the decoded QR-code data */
-struct QRCodeData
-{
-  bool valid;
-  int dataType;
-  uint8_t payload[1024];
-  int payloadLen;
-};
 
 class ESP32QRCodeReader
 {
-private:
-  TaskHandle_t qrCodeTaskHandler;
-  CameraPins pins;
-  framesize_t frameSize;
-
 public:
-  camera_config_t cameraConfig;
-  QueueHandle_t qrCodeQueue;
-  bool begun = false;
-  bool debug = false;
 
   // Constructor
   ESP32QRCodeReader();
-  ESP32QRCodeReader(CameraPins pins);
-  ESP32QRCodeReader(CameraPins pins, framesize_t frameSize);
-  ESP32QRCodeReader(framesize_t frameSize);
-  ~ESP32QRCodeReader();
+  ~ESP32QRCodeReader() { 
+    quirc_destroy(this->q);
+  }
 
-  // Setup camera
-  QRCodeReaderSetupErr setup();
+  uint8_t buffer[1024];
 
-  void begin();
-  void beginOnCore(BaseType_t core);
-  bool receiveQrCode(struct QRCodeData *qrCodeData, long timeoutMs);
-  void end();
+  bool qrCodeDetectTask(camera_config_t* camera_config, camera_fb_t *fb);
 
-  void setDebug(bool);
+private:
+  struct quirc *q = NULL;
+  uint16_t old_width = 0;
+  uint16_t old_height = 0;
+
 };
 
 #endif // ESP32_QR_CODE_ARDUINO_H_
