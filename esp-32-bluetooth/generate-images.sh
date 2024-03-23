8e3bc5223df5852b5e3f25ba073cb6d1ba58cdf3
Matus Ujhelyi <matus.ujhelyi@tind.sk>
2024-03-23 23:01:58 +0100

    update build process

--- esp-32-bluetooth/generate-images.sh
+++ esp-32-bluetooth/generate-images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $# != "1" ]; then
-    echo "You need to provide arduino build number"
-    exit 1
-fi
+#if [ $# != "1" ]; then
+#    echo "You need to provide arduino build number"
+#    exit 1
+#fi
 
 rm images.zip
 
-zip -j images.zip /home/matus/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/partitions/boot_app0.bin /home/matus/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/sdk/bin/bootloader_qio_80m.bin /tmp/arduino_build_$1/serial_to_bt.ino.bin /tmp/arduino_build_$1/serial_to_bt.ino.partitions.bin flash-from-cmdline
+zip -j images.zip /home/matus/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/partitions/boot_app0.bin /home/matus/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/sdk/bin/bootloader_qio_80m.bin /tmp/arduino_build/serial_to_bt.ino.bin /tmp/arduino_build/serial_to_bt.ino.partitions.bin flash-from-cmdline
